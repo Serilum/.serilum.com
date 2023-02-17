53bb1212f7c7bf75cc6e0b49dd00e3c46fe8397e
Rick South <rick@natamus.com>
2023-02-17 21:18:07 +0100

    Updated the global changelog page

--- assets/js/changelog.js
+++ assets/js/changelog.js
@@ -13,7 +13,7 @@ function loadChangelogData() {
 			clhtml += '<div class="header">';
 			clhtml += "<h1>Global Mod Changelog</h1>";
 			clhtml += "<p>This page contains an overview of all changelog submitted. An easy way to see if your favourite mod has been updated lately.</p>";
-			clhtml += "<p>You can click the mod names to look at the current source code on Github.</p><br>";
+			clhtml += "<p>You can click the mod names to look at the complete changelog.</p><br>";
 			clhtml += "<p>If you use Discord, there's also a channel available which receives a message after each mod update:</p><br>";
 			clhtml += '<p><a href="https://ntms.link/changelog" target=_blank><img src="/assets/images/discord_mod-changelog.png"></a></p>';
 			clhtml += '<p><a class="discordlink" href="https://ntms.link/changelog" target=_blank>https://ntms.link/changelog</a></p>';
@@ -30,7 +30,7 @@ function loadChangelogData() {
 					lasttimestamp = linedata["timestamp"];
 				}
 
-				line += '<p class="modname"><a href="https://serilum.com/source-code/?mod=' + linedata["slug"] + '" target=_blank>' + linedata["modname"] + ' (' + linedata["version"] + ')</a></p>';
+				line += '<p class="modname"><a href="https://serilum.com/mods/?changelog=' + linedata["slug"] + '" target=_blank>' + linedata["modname"] + ' (' + linedata["version"] + ')</a></p>';
 				line += '<p class="separator">=========</p>'
 				line += '<p class="content">' + linedata["content"].replaceAll("\n", "<br>") + '</p>'
 
