8168f753786bdab05c9eb5edf7b5a99740fce7ed
Rick South <rick@natamus.com>
2023-12-12 16:37:56 +0100

    Updated content

---
+++ assets/js/issue-tracker.js
@@ -0,0 +1,6 @@
+$(document).ready(function(e) {
+	//let url = document.URL;
+	//let slug = url.split("/mod/")[1]
+
+	window.location.assign("https://github.com/ricksouth/serilum-mc-mods/issues");
+});
