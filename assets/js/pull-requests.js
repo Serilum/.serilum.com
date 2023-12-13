2210037888d0effe4edccfc8d0c0af73c9f3da1e
Rick South <rick@natamus.com>
2023-12-13 18:16:24 +0100

    Update pull request redirect reference to new location.

--- assets/js/pull-requests.js
+++ assets/js/pull-requests.js
@@ -1,3 +1,3 @@
 $(document).ready(function(e) {
-	window.location.replace("https://github.com/ricksouth/serilum-mc-mods/wiki/Pull-Request-Information");
+	window.location.replace("https://github.com/Serilum/.information/wiki/Pull-Request-Information");
 });
